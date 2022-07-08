90c89653ee4269339cb629492509bc7ff14eb2c6
vilmeyr@gmail.com <vilmey.filho@eldorado.org.br>
2022-07-08 15:11:56 -0300

    Adding base project
    Project configured to be built for the target controller and for
    host computer.

---
+++ Unity/HelloWorld_NoTest/source/hello_world.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TEST
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+
+#else
+
+#include "stdio.h"
+
+#endif
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+#ifndef TEST
+
+    char ch;
+    /* Init board hardware. */
+    BOARD_ConfigMPU();
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+
+    PRINTF("hello world.\r\n");
+
+    while (1)
+    {
+        ch = GETCHAR();
+        PUTCHAR(ch);
+    }
+
+#else
+    printf("\n\nHello World\n\n");
+    getchar();
+
+#endif
+}
